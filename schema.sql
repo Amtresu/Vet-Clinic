27328fca63ffebf1c21e7cdd74333cdad725fbb0
Amtresu <conor.follette@gmail.com>
2022-10-18 09:44:26 +0900

    Added database, table, data, and queries.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals ( 
+	id INT PRIMARY KEY NOT NULL,
+	name text,
+	date_of_birth date,
+	escapte_attempts int,
+	neutered boolean, 
+	weight_kg decimal
 );
